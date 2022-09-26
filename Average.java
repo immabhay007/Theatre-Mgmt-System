afee61caf65ba97312a8f5bfe6925cc42e87233c
immabhay007 <114064999+immabhay007@users.noreply.github.com>
2022-09-26 10:09:05 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,23 @@
+
+public class Average {
+
+	public static void main(String[] args) {
+	
+		int s1 = 66;
+		int s2 = 72;
+		int s3 = 20;
+		int s4 = 34;
+		int s5 = 55;
+		
+		int average = (s1+s2+s3+s4+s5)/5 ;
+		
+		if (average < 40) {
+			System.out.println("You are fail");
+		}
+		else
+		System.out.println("You are pass");
+	
+
+	}
+
+}
